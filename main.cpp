d5e0c94fcd2afc1f2e85b232d2c09a5c56f77ee4
AbuzarN <abuzarnaqvi17@gmail.com>
2023-11-14 19:49:11 -0600

    phase 1

--- main.cpp
+++ main.cpp
@@ -23,8 +23,83 @@ int main()
             cube.D();
         }
         //cube.display_cube();
+        std::cout<<w;
         std::cout<<"bottom scanned"<<std::endl;
 
+        if (w<4)
+        {
+            for (int i =0; i<4;i++)
+            {
+                if (cube.top[2][1]=='W')
+                {
+                    //std::cout<<std::endl<<"Found white"<<std::endl;
+                    w++;
+                    cube.F();cube.F();cube.D();
+                    while (cube.top[2][1] =='W'){cube.F();cube.F();cube.D();}
+                }
+                cube.U();
+            }
+        }
+        cube.display_cube();
+        std::cout<<w;
+        std::cout<<"top scanned"<<std::endl;
+
+        if (w<4)
+        {
+            for (int i =0; i<4 && w<4; i++)
+            {
+                if (cube.front [1][0]=='W')
+                {
+                    w++;
+                    cube.L();cube.D();cube.Lp();
+                    if (cube.front [1][0]=='W' && w==4){cube.L();}
+                    while (cube.front [1][0]=='W'&& w!=4){cube.L();cube.D();cube.Lp();}
+                }
+                if (cube.front [1][2]=='W' && w<4)
+                {
+                    w++;
+                    cube.Rp();cube.D();cube.R();
+                    if (cube.front [1][2]=='W'&& w==4){cube.Rp();}
+                    while (cube.front [1][2]=='W' && w!=4){cube.Rp();cube.D();cube.R();}
+                }
+             //cube.display_cube();
+                cube.turn_cube_left();
+            }
+            
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i =0; i<4; i++)
+                {
+                    if (cube.front[1][0]=='W' || cube.front[1][2]=='W' )
+                    {
+                        flag = true;
+                        break;
+                    }
+                    cube.turn_cube_left();
+                }
+                if (flag && cube.front [1][0]=='W')
+                {
+                     w++;
+                    cube.L();cube.D();cube.Lp();
+                    if (cube.front [1][0]=='W'&& w==4){cube.L();}
+                    while (cube.front [1][0]=='W'&& w!=4){cube.L();cube.D();cube.Lp();}
+                }
+                else if (flag && cube.front [1][2]=='W')
+                {
+                    w++;
+                    cube.Rp();cube.D();cube.R();
+                    if (cube.front [1][2]=='W'&& w==4){cube.Rp();}
+                    while (cube.front [1][2]=='W' && w!=4){cube.Rp();cube.D();cube.R();}
+                }
+
+            }
+            
+        }
+        cube.display_cube();
+        std::cout<<w;
+        std::cout<<"row 2 scanned"<<std::endl;
 
         if (w<4)
         {
@@ -35,9 +110,9 @@ int main()
                     w++;
                     cube.L();cube.L();cube.F();cube.L();cube.Fp();
                     if (cube.front[2][1]=='W'){cube.F();cube.F();}
-                    while (cube.front[0][1] =='W'){cube.F();cube.F();cube.D();cube.L();cube.L();cube.Fp();cube.L();cube.F();}
+                    while (cube.front[0][1] =='W'){cube.D();cube.L();cube.L();cube.Fp();cube.L();cube.F();}
                 }
-                cube.U();
+                cube.D();
                 /*
                 std::cout<<std::endl;
                 cube.display_cube();
@@ -45,33 +120,9 @@ int main()
 
             }
         }
-        std::cout<<"row 3 scanned"<<std::endl;
-
-        if (w<4)
-        {
-            for (int i =0; i<4 && w<4; i++)
-            {
-                if (cube.front [1][0]=='W')
-                {
-                    w++;
-                    cube.L();cube.D();cube.Lp();
-                    if (w==4){cube.L();}
-                    while (cube.front [1][0]=='W'&& w!=4){cube.L();cube.D();cube.Lp();}
-                }
-                if (cube.front [1][2]=='W' && w<4)
-                {
-                    w++;
-                    cube.Rp();cube.D();cube.R();
-                    if (w==4){cube.Rp();}
-                    while (cube.front [1][2]=='W' && w!=4){cube.Rp();cube.D();cube.R();}
-                }
-            // cube.display_cube();
-                cube.turn_cube_left();
-            }
-            
-        }
-        //cube.display_cube();
-        std::cout<<"row 2 scanned"<<std::endl;
+        cube.display_cube();
+        std::cout<<w;
+        std::cout<<"row 3 scanned"<<std::endl;   
 
         if (w<4)
         {
@@ -91,27 +142,12 @@ int main()
 
             }
         }
-        //cube.display_cube();
-        
+        cube.display_cube();
+        std::cout<<w;
         std::cout<<"row 1 scanned"<<std::endl;
 
         
-    if (w<4)
-        {
-            for (int i =0; i<4;i++)
-            {
-                if (cube.top[2][1]=='W')
-                {
-                    std::cout<<std::endl<<"Found white"<<std::endl;
-                    w++;
-                    cube.F();cube.F();cube.D();
-                    while (cube.top[2][1] =='W'){cube.F();cube.F();cube.D();}
-                }
-                cube.U();
-            }
-        }
-        //cube.display_cube();
-        std::cout<<"top scanned"<<std::endl;
+    
     cube.display_cube();
     
     return 0;
